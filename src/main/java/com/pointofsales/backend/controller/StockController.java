a653055bb66fc31f7a6dd3c731f30a0d52fe27df
Kermit Song <kermit.song@qbs.dev>
2019-10-07 23:35:48 +0800

    import controller
    unfinished yet

--- src/main/java/com/pointofsales/backend/controller/StockController.java
+++ src/main/java/com/pointofsales/backend/controller/StockController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -28,5 +30,10 @@ public class StockController {
     @DeleteMapping("/stocks/{stockId}")
     public void deleteStocks(@PathVariable long stockId) {
         stockRepository.deleteById(stockId);
+    }
+
+    @PostMapping("/stocks/import")
+    public void importStocks(@RequestParam MultipartFile file) throws IOException {
+        System.out.println(file.getInputStream());
     }
 }
